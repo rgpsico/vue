2fd9e3d2d1b46ea3505bf252f814bd8ce3560ede
rgpsico <gerro121@hotmail.com>
2022-01-29 01:29:44 -0300

    ajustando o modal no checkout

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,4 +1,4 @@
-export const URI_BASE_API = 'https://api.ppgfood.com.br/api'
+export const URI_BASE_API = 'https://api.ppgfood.com.br/api/'
 export const API_VERSION =  'v1'
 export const TOKEN_NAME = 'token_sanctum'
 
