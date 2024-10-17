71453550efd3bd90c0c275d164bb73539a2af2c1
roger <rogernevesn@gmail.com>
2024-10-17 15:22:34 -0300

    ajustes

--- dist/js/chunk-d0f1e0c0.e06502fc.js
+++ dist/js/chunk-d0f1e0c0.e06502fc.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-d0f1e0c0"],{"23d7":function(n,t,c){"use strict";c("62a1")},"62a1":function(n,t,c){},b320:function(n,t,c){"use strict";c.r(t);var e=function(){var n=this,t=n.$createElement,c=n._self._c||t;return c("div",[c("div",{staticClass:"container container-body"},[c("router-view")],1)])},i=[],a=(c("23d7"),c("2877")),o={},r=Object(a["a"])(o,e,i,!1,null,"074a0c2c",null);t["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-d0f1e0c0.851f7aae.js.map
+//# sourceMappingURL=chunk-d0f1e0c0.e06502fc.js.map
