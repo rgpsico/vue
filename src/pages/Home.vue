7c5a464c26b8129bcc8dd0c4bc1a5b99e3ce56c5
roger <rogernevesn@gmail.com>
2025-04-09 02:17:47 -0300

    teste

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,102 +1,93 @@
 <template>
-     <div class="row">
+  <div class="row">
+    <div class="col-lg-12">
+      <h1 class="my-5 title-tenant">Restaurantes</h1>
 
-      <div class="col-lg-12">
-
-        <h1 class="my-5 title-tenant">Restaurantes</h1>
-
-        <div class="row my-4">
-
-          <div class="col-lg-4 col-md-5 col-12 mb-4 ml-4" v-for="(company , index) in companies.data" :key="index">
-        
-            <div class="restaurant-card">
-                <a href="#"  @click.prevent="goStoreCompany(company)">       
-              <img 
-              v-if="company.image"
-              class="card-img-top"
-              :src="company.image"
-              :alt="company.name"
-               :height="300"
+      <div class="row my-4">
+        <div
+          class="col-lg-4 col-md-5 col-12 mb-4 ml-4"
+          v-for="(company, index) in companies.data"
+          :key="index"
+        >
+          <div class="restaurant-card">
+            <a href="#" @click.prevent="goStoreCompany(company)">
+              <img
+                v-if="company.image"
+                class="card-img-top"
+                :src="company.image"
+                :alt="company.name"
+                :height="300"
                 :width="80"
-              
-              >
+              />
 
-                <img v-else
-                  class="card-img-top"
-                  src="@/assets/imgs/vue-food.png"
-                  :alt="company.name"
-                  :height="80"
-                  :width="80"
-                  >
-              </a>
-                <div class="restaurant-card-body">
-                <h3>
-               <!-- <router-link 
-                :to="{name:'products', 
-                params:{companyFlag:company.flag}}">
-                  {{company.name}}
-                </router-link> -->
+              <img
+                v-else
+                class="card-img-top"
+                src="@/assets/imgs/vue-food.png"
+                :alt="company.name"
+                :height="80"
+                :width="80"
+              />
+            </a>
+            <div class="restaurant-card-body">
+              <h3>
+                <router-link
+                  :to="{
+                    name: 'products',
+                    params: { companyFlag: company.flag },
+                  }"
+                >
+                  {{ company.name }}
+                </router-link>
 
                 <a href="#" @click.prevent="goStoreCompany(company)">
-                    {{company.name}}
-
+                  {{ company.name }}
                 </a>
-                </h3>
-              </div>
+              </h3>
             </div>
           </div>
-
-         
         </div>
-        <!-- /.row -->
-
       </div>
-      <!-- /.col-lg-9 -->
-
+      <!-- /.row -->
     </div>
-    <!-- /.row -->
+    <!-- /.col-lg-9 -->
+  </div>
+  <!-- /.row -->
 </template>
 
 <script>
-import {mapActions , mapState , mapMutations} from 'vuex'
+import { mapActions, mapState, mapMutations } from "vuex";
 
 export default {
   mounted() {
-    this.getCompanies()
-    .catch(response => this.$vToastify.error("Falha ao carregar empresas","Erro"))
-    
-},
+    this.getCompanies().catch((response) =>
+      this.$vToastify.error("Falha ao carregar empresas", "Erro")
+    );
+  },
 
   computed: {
- //   companies() {
-   //   return this.$store.state.companies.items
-  //  }
-  ...mapState({
-    companies: state => state.companies.items
-
-  })
-
+    //   companies() {
+    //   return this.$store.state.companies.items
+    //  }
+    ...mapState({
+      companies: (state) => state.companies.items,
+    }),
   },
   methods: {
-    ...mapActions([
-      'getCompanies'
-      
-    ]),
+    ...mapActions(["getCompanies"]),
 
     ...mapMutations({
-      setCompany: 'SET_COMPANY_SELECTED'
-
-
+      setCompany: "SET_COMPANY_SELECTED",
     }),
 
-    goStoreCompany (company) {
-    this.setCompany(company)
+    goStoreCompany(company) {
+      this.setCompany(company);
 
-    this.$router.push({name:'products' , 
-    params: {companyFlag:company.flag} 
-    })
-
-    }
-  }
-}
+      this.$router.push({
+        name: "products",
+        params: { companyFlag: company.flag },
+      });
+    },
+  },
+};
 </script>
