725bc983b71151a08baf1ec3eca5efcc7c8f2a6f
roger <rogernevesn@gmail.com>
2025-04-09 02:13:29 -0300

    teste

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,165 +1,150 @@
 <template>
-<div>
- <h1 class="my-4 title-tenant text-center">{{company.name}}</h1>
-   
-    <div class="row">      
-        <div class="list-categories ">
-          <a href="#"
-              :class="['list-categories__item ',categoryInFilter('')]"
-               @click.prevent="filterByCategory('')"
-              >
-            <div class="icon">
-              <i class="fas fa-pizza-slice"></i>
-            </div> 
-            <span> Todas </span>
-          </a>
+  <div>
+    <h1 class="my-4 title-tenant text-center">{{ company.name }}</h1>
 
-  
-          <a href="#"
+    <div class="row">
+      <div class="list-categories ">
+        <a
+          href="#"
+          :class="['list-categories__item ', categoryInFilter('')]"
+          @click.prevent="filterByCategory('')"
+        >
+          <div class="icon">
+            <i class="fas fa-pizza-slice"></i>
+          </div>
+          <span> Todas </span>
+        </a>
+
+        <a
+          href="#"
           @click.prevent="filterByCategory(category.identify)"
-       
-          v-for="(category , index) in categories.data" 
+          v-for="(category, index) in categories.data"
           :key="index"
-            :class="['list-categories__item ',categoryInFilter(category)]" 
-          >
-            <div class="icon">
-              <i class="fas fa-pizza-slice"></i>
-            </div> 
-            <span> {{category.name}} </span>
-          </a>
-        </div>
+          :class="['list-categories__item ', categoryInFilter(category)]"
+        >
+          <div class="icon">
+            <i class="fas fa-pizza-slice"></i>
+          </div>
+          <span> {{ category.name }} </span>
+        </a>
+      </div>
     </div>
 
     <div class="row my-4">
       <div v-if="company.products.data.length === 0">
         nenhum produto encontrado
       </div>
-
     </div>
 
-     <div class="row my-4" >
-      <div class="col-lg-4 col-md-6 mb-4"
-     v-for="(product, index) in company.products.data" :key="index">
-        <div :class="['card-flat','h-100', {'disabled': productInCart(product)}]">
+    <div class="row my-4">
+      <div
+        class="col-lg-4 col-md-6 mb-4"
+        v-for="(product, index) in company.products.data"
+        :key="index"
+      >
+        <div
+          :class="['card-flat', 'h-100', { disabled: productInCart(product) }]"
+        >
           <a href="#">
             <div class="card-image">
-              <img class="card-img-top" :src="product.image" alt="">
+              <img class="card-img-top" :src="product.image" alt="" />
             </div>
           </a>
           <div class="card-body">
             <h4 class="card-title">
-              <a href="#">{{product.title}} </a>
+              <a href="#">{{ product.title }} </a>
             </h4>
-            <h5>R$ {{product.price}}</h5>
-            <p class="card-text">{{product.description}}</p>
+            <h5>R$ {{ product.price }}</h5>
+            <p class="card-text">{{ product.description }}</p>
           </div>
           <div class="card-footer card-footer-custom">
-            <a href="#" @click.prevent="addProdCart(product)">Adicionar no Carrinho <i class="fas fa-cart-plus"></i></a>
+            <a href="#" @click.prevent="addProdCart(product)"
+              >Adicionar no Carrinho <i class="fas fa-cart-plus"></i
+            ></a>
           </div>
         </div>
       </div>
-
-     
-     
     </div>
-  
-    </div>
-    <!-- /.row -->
+  </div>
+  <!-- /.row -->
 </template>
 
 <script>
-import {mapState , mapActions , mapMutations } from 'vuex'
+import { mapState, mapActions, mapMutations } from "vuex";
 
 export default {
-  props:['companyFlag'],
+  props: ["companyFlag"],
 
-  created() {   
-    if (this.company.name === '')  {
-        return this.$router.push({name:'home'})
+  created() {
+    if (this.company.name === "") {
+      return this.$router.push({ name: "home" });
     }
 
-     this.getCategoriesByCompany(this.company.uuid)
-    .catch(response => this.$vToastify.error("Falha ao carregar categorias","Erro"))
-    
-  
-   //  this.getProductsByCompany(this.company.uuid)
-    //.catch(response => this.$vToastify.error("Falha ao carregar produtos","Erro"))
-       this.loadProducts()
+    this.getCategoriesByCompany(this.company.uuid).catch((response) =>
+      this.$vToastify.error("Falha ao carregar categorias", "Erro")
+    );
+
+    this.loadProducts();
   },
 
   computed: {
     ...mapState({
-      company: state     => state.companies.companySelected,
-      categories: state  => state.companies.categoriesCompanySelected,
-      productsCart:state => state.cart.products
-     }),
+      company: (state) => state.companies.companySelected,
+      categories: (state) => state.companies.categoriesCompanySelected,
+      productsCart: (state) => state.cart.products,
+    }),
   },
 
-data() {
-  return {
-    filters: {
-      category: ''
-    }
-  }
-},
-
+  data() {
+    return {
+      filters: {
+        category: "",
+      },
+    };
+  },
 
   methods: {
-    ...mapActions([
-      'getCategoriesByCompany',
-      'getProductsByCompany',
-      
-    ]),
+    ...mapActions(["getCategoriesByCompany", "getProductsByCompany"]),
 
-      ...mapMutations({
-      addProdCart: 'ADD_PRODUCT_CART',
-      removeTableCompany: 'REMOVE_TABLE_COMPANY',
-      removeCompany: 'REMOVE_COMPANY_SELECTED',
-  
-
+    ...mapMutations({
+      addProdCart: "ADD_PRODUCT_CART",
+      removeTableCompany: "REMOVE_TABLE_COMPANY",
+      removeCompany: "REMOVE_COMPANY_SELECTED",
     }),
-    
-    loadProducts () {
+
+    loadProducts() {
       const params = {
         token_company: this.company.uuid,
-      }
+      };
 
       if (this.filters.category) {
-        params.categories = [
-          this.filters.category
-        ]
-      } 
+        params.categories = [this.filters.category];
+      }
 
-      this.getProductsByCompany(params)
-      .catch(response=> this.$vToastify.error('falha ao carregar os produtos','Erro'))  
-    
-  },
+      this.getProductsByCompany(params).catch((response) =>
+        this.$vToastify.error("falha ao carregar os produtos", "Erro")
+      );
+    },
 
     filterByCategory(identify) {
-    this.filters.category = identify
-    this.loadProducts()
+      this.filters.category = identify;
+      this.loadProducts();
+    },
+
+    categoryInFilter(identify) {
+      return identify === this.filters.category ? "active" : "";
+    },
+
+    productInCart(product) {
+      let inCart = false;
+      this.productsCart.map((prodCart, index) => {
+        if (prodCart.identify === product.identify) {
+          inCart = true;
+        }
+      });
+
+      return inCart;
+    },
   },
-
-  categoryInFilter (identify)
-  {
-    return identify ===   this.filters.category ? 'active' : '';
-  },
-
-  productInCart(product) {
-    let  inCart = false
-   this.productsCart.map((prodCart, index) => {
-     if( prodCart.identify === product.identify ) {
-       inCart = true
-     } 
-   }) 
-
-return inCart
-
-  }
-  
-
-
- }
- 
-}
+};
 </script>
